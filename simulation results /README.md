0688afdc8cee683526444c591cf543c5941724a6
Marianna R <rapsomaniki.marianna@gmail.com>
2020-03-19 13:56:30 +0100

    Added input data, simulation results and figure generating scripts

---
+++ simulation results /README.md
@@ -0,0 +1,9 @@
+## Simulation results  
+The simulated DNA replication prifiles are organized in the following folders:
+* runA_ran: first simulation run - model variant 1 - random origin positioning
+* runA_spb: first simulation run - model variant 2 - realistic origin positioning
+* runA_uni: first simulation run - model variant 3 - SPB-mediated particle activation
+* runB: second simulation run - model variant 3 - SPB-mediated particle activation
+
+Each folder contains a number of `.csv` files, where DNA replication completion times and origin firing times are recorded. The simulation parameters are indicated in the filename, where `N` the number of particles, `D` the diffusion coefficient, `R` the attraction radius, `P` the binding probability and `A` the activation probability. 
+
---
+++ simulation results /runA_uni/run.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+total_duration=0
+current_iteration=0
+for n_particles in 10 50 100 150 200 400 600 800 1000
+do
+	for d_coef in "0.5" "2.0" "3.5"
+	do
+		for r_attr in "0.015" "0.025" "0.035"
+		do
+			for p_bind in 1
+			do
+				for p_act in 1
+				do
+					t_start=$(date +%s.%N)
+					current_iteration=$((current_iteration + 1))
+					out_key="SIM_N${n_particles}_D${d_coef}_R${r_attr}_P${p_bind}"
+					mpirun /u/amp/jonas/dnarepl-batch -o /u/amp/simulations/setup/data/oripos_heichinger.csv -c /u/amp/simulations/setup/data/contig_wood50.csv -k $out_key -s /u/amp/simulations/setup/data/G1_grand_eve -w /u/amp/simulations/runA_uni/out -i 5 -g 0.02 -r 1.33 -x 1.54 -f 50 -n $n_particles -d $d_coef -p $p_bind -b $r_attr -a $p_act >> /u/amp/simulations/runA_uni/error
+					iteration_duration=$(echo "$(date +%s.%N) - ${t_start}" | bc)
+					total_duration=$(echo "${total_duration} + ${iteration_duration}" | bc)
+				done
+			done
+		done
+	done
+done
